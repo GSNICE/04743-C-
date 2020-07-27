566813cbab547ef88326aee8127716a700c6759a
wewin-tech <936281005@qq.com>
2020-07-27 18:41:41 +0800

    add 2-3.cpp

--- chapter2/2-1.cpp
+++ chapter2/2-1.cpp
@@ -1,51 +1,59 @@
-
 class myDate
 {
 public:
-	myDate();	//构造函数
-	myData(int, int ,int); //构造函数
-	void setDate(int, int, int); //设置日期
-	void setDate(myDate); 	//设置日期
-	myDate getDate();	//获取日期
-	void setYear(int); //设置年
-	int getMonth();	//获取月
-	void printDate()const; //打印日期
+    myDate();                                       //构造函数
+    myDate(int, int, int);          //构造函数
+    void setDate(int, int, int);//设置日期
+    void setDate(myDate);           //设置日期
+    myDate getDate();                       //获取日期
+    void setYear(int);                      //设置年
+    int getMonth();                         //获取年
+    void printDate()const;          //打印日期
 private:
-	int year, month, day;	//成员变量，表示年、月、日
+    int year, month, day;           //成员变量,表示年，月，日
 };
-//在类体外定义成员函数
+//在类外定义成员函数
 myDate::myDate()
 {
-	year = 1970, month = 1, day = 1;
+    year = 1970; month = 1; day = 1;
 }
+
 myDate::myDate(int y, int m, int d)
 {
-	year = y; month = m; day = d;
+    year = y; month = m; day = d;
 }
+
 void myDate::setDate(int y, int m, int d)
 {
-	year = y; month = m; day = d;
+    year = y; month = m; day = d;
+    return;
 }
-void myData::setDate(myDate oneD)
+
+void myDate::setDate(myDate oneD)
 {
-	year = oneD.year, month = oneD.month, day = oneD.day;
+    year = oneD.year; month = oneD.month; day = oneD.day;
+    return;
 }
+
 myDate myDate::getDate()
 {
-	return *this;
+    return *this;
 }
+
 void myDate::setYear(int y)
 {
-	year = y;
-	return;
+    year = y;
+    return;
 }
+
 int myDate::getMonth()
 {
-	return month();
+    return month;
 }
+
 void myDate::printDate()const
 {
-	cout << year << "/" << month << "/" << day;
-	return;
+    cout << year << "/" << month << "/" << day;
+    return;
 }
 
--- chapter2/2-2.cpp
+++ chapter2/2-2.cpp
@@ -1,46 +1,49 @@
 class Student
 {
 public:
-	void setStudents(string, myDate);	//设置学生信息
-	void setName(string);				//设置姓名
-	string getName();					//获取姓名		
-	void setBirthday(myDate);			//设置生日
-	myDate gitBirthday();				//获取生日
-	void printStudent()const; 			//打印信息
+    void setStudents(string, myDate);       //设置学生信息
+    void setName(string);                           //设置姓名
+    string getName();                                       //获取姓名
+    void setBirthday(myDate);                       //设置生日
+    myDate getBirthday();                           //获取生日
+    void printStudent()const;                       //打印信息
 private:
-	string name; 		//姓名
-	myDate birthday;	//生日
+    string name;            //姓名
+    myDate birthday;        //生日
 };
 //在类体外定义成员函数
-void Student::setStudent(string s, myDate d)
+void Student::setStudents(string s, myDate d)
 {
-	name = s;
-	birthday.setDate(d);
-	return;
-}
-void Student::setName(string n)
-{
-	name = n;
-	return;
-}
-string Student::getName()
-{
-	return name;
-}
-void Student::setBirthday(my Date d)
-{
-	birthday.setDate(d);
-	return;
-}
-myDate Student::getBirthday()
-{
-	return birthday;
-}
-void Student::printStudent()const
-{
-	cout << "姓名：" << name << "\t生日：";
-	birthday.printDate();	//调用类myDate的成员函数
-	cout << endl;
+    name = s;
+    birthday.setDate(d);
+    return;
 }
 
-	
+void Student::setName(string n)
+{
+    name = n;
+    return;
+}
+
+string Student::getName()
+{
+    return name;
+}
+
+void Student::setBirthday(myDate d)
+{
+    birthday.setDate(d);
+    return;
+}
+
+myDate Student::getBirthday()
+{
+    return birthday;
+}
+
+void Student::printStudent()const
+{
+    cout << "姓名：" << name << "\t生日：";
+    birthday.printDate();   //调用类myDate的成员函数
+    cout << endl;
+}
