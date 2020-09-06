7e49fc5045cc127c72a14d1717c34c939871f1d0
wewin-tech <936281005@qq.com>
2020-09-06 11:09:49 +0800

    add 9-14.cpp

---
+++ chapter9/9-14.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+template<class T>
+class TBase
+{
+	T data1;
+public:
+	void print()
+	{
+		cout << "TBase::" << data1 << endl;
+	}
+};
+template<class T2>
+class TDerived:public TBase<int> //类模板继承于模板类
+{
+	T2 data2;
+public:
+	void print()
+	{
+		TBase<int>::print();
+		cout << "TDerived::" << data2 << endl;
+	}
+};
+
+int main()
+{
+	TDerived<int>d;
+	d.print();
+	TDerived<string>d2;
+	d2.print();
+}
+
+
